756bc7d14aab0225f592a430420956f4ee576b93
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-01-20 10:50:41 +0530

    recursion

--- src/com/sahil/recursion.java
+++ src/com/sahil/recursion.java
@@ -1,4 +1,16 @@
 package com.sahil;
 
 public class recursion {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    private static void print(int i) {
+        if(i == 5){
+            return;
+        }
+        System.out.println(i);
+        print(i+1);
+    }
+
 }
