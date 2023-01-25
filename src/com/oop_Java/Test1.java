d15eea8e107caede587d0246de3eb0a8dd40ad91
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-01-25 11:13:08 +0530

    oop

---
+++ src/com/oop_Java/Test1.java
@@ -0,0 +1,9 @@
+package com.oop_Java;
+
+public class Test1 extends Test2{
+    public static void main(String[] args) {
+    }
+    void run(){
+        System.out.println("run");
+    }
+}
---
+++ src/com/oop_Java/Test2.java
@@ -0,0 +1,11 @@
+package com.oop_Java;
+
+public class Test2 {
+    Test2 t2 = new Test2();
+
+
+    public static void main(String[] args) {
+        Test1 t1 = new Test1();
+        t1.run();
+    }
+}
