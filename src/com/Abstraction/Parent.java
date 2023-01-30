cd69d8dc2690005daa21f3fc93d8c520d8ee06cb
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-01-30 10:40:23 +0530

    linked list

--- src/com/Abstraction/Parent.java
+++ src/com/Abstraction/Parent.java
@@ -1,6 +1,9 @@
 package com.Abstraction;
 
 public abstract class Parent {
+    public Parent(){
+        System.out.println("Abstract Parent constructor");
+    }
     abstract void demo();
     public void display(){
         System.out.println("Concreate method");
--- src/com/Abstraction/child.java
+++ src/com/Abstraction/child.java
@@ -1,6 +1,9 @@
 package com.Abstraction;
 
 public class child extends Parent{
+    child(){
+        System.out.println("Child Constructor");
+    }
     public void demo(){
         System.out.println("Demo implmented here extended from Parent class ;");
 
