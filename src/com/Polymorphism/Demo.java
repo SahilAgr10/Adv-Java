e8663128a71d1cf20ac2a6efbdbf330c81896362
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-01-30 11:59:56 +0530

    linked list

---
+++ src/com/Polymorphism/Demo.java
@@ -0,0 +1,10 @@
+package com.Polymorphism;
+
+public class Demo {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.getData();
+        e.getData(5);
+        e.getData("Samurai");
+    }
+}
---
+++ src/com/Polymorphism/Employee.java
@@ -0,0 +1,17 @@
+package com.Polymorphism;
+
+public class Employee {
+    void getData(){
+        System.out.println("No parameter method");
+    }
+    void getData(int a){
+        System.out.println("Method with int type");
+    }
+    void getData(String ans){
+        System.out.println("Method with string Parameter");
+    }
+    void getData(float b) {
+        System.out.println("Method with type float");
+    }
+
+}
