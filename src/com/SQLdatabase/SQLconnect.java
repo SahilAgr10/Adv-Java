3fe4f8fc223c0c1294b02e92e7660879fd89f62a
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-02 10:44:35 +0530

    SQL database connection

--- src/com/SQLdatabase/SQLconnect.java
+++ src/com/SQLdatabase/SQLconnect.java
@@ -1,15 +1,22 @@
 package com.SQLdatabase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQLconnect {
     //jdbc Demo
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         String driver = "com.mysql.jdbc.Driver";
         String user="root";
         String pass= "1234";
         String url = "jdbc:mysql://127.0.0.1:3306/super30";
 
-        //Class.forName(driver);
+        Class.forName(driver);
+
+        Connection connection = DriverManager.getConnection(url,user,pass);
+        System.out.println("Connecting with mysql..");
 
     }
 }
