8e873d455d626c1869da1a927c078691e057bb68
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-03 11:04:06 +0530

    Collection framework

--- src/com/CollectionFramework/Arraylst.java
+++ src/com/CollectionFramework/Arraylst.java
@@ -5,11 +5,25 @@ import java.util.Arrays;
 
 public class Arraylst {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList arrayList = new ArrayList<>();
+        // NON-Generic Collection where type is not defined
         arrayList.add(23);
         arrayList.add(45);
         arrayList.add(56);
+        arrayList.add("Pune");
+        arrayList.add(56);
         System.out.println(arrayList.size());
         System.out.println(arrayList);
+        System.out.println(arrayList.indexOf(56));
+
+
+        ArrayList<Integer> arrayLst = new ArrayList<>();
+        //Generic Collection where type is defined
+        arrayLst.add(23);
+        arrayLst.add(45);
+        arrayLst.add(56);
+        System.out.println(arrayLst);
+
+
     }
 }
