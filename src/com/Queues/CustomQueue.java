e29a9ef6386248668b0984c7e01542fb7bb37b31
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-03 19:20:08 +0530

    Custom Queue Implementation

--- src/com/Queues/CustomQueue.java
+++ src/com/Queues/CustomQueue.java
@@ -18,6 +18,34 @@ public class CustomQueue {
         data[end++] = item;
         return true;
     }
+    public int remove() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+
+        int removed = data[0];
+
+        // shift the elements to left
+        for (int i = 1; i < end; i++) {
+            data[i-1] = data[i];
+        }
+        end--;
+        return removed;
+    }
+
+    public int front() throws Exception{
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+        return data[0];
+    }
+
+    public void display() {
+        for (int i = 0; i < end; i++) {
+            System.out.print(data[i] + " <- ");
+        }
+        System.out.println("END");
+    }
 
     public boolean isFull() {
         return end == data.length; // ptr is at last index
