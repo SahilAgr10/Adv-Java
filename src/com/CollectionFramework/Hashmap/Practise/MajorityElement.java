41d9c5c78b785ef32190a8c4c93265ace4fa1d52
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-08 12:42:47 +0530

    Hashmap Implementation

--- src/com/CollectionFramework/Hashmap/Practise/MajorityElement.java
+++ src/com/CollectionFramework/Hashmap/Practise/MajorityElement.java
@@ -1,8 +1,24 @@
 package com.CollectionFramework.Hashmap.Practise;
 import java.util.*;
 public class MajorityElement {
+    public static void majorityElement(int[] nums){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0 ;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            } else {
+                map.put(nums[i],1);
+            }
+        }
+        for(int key : map.keySet()){
+            if(map.get(key) > n/9){
+                System.out.println(key);
+            }
+        }
+    }
     public static void main(String[] args) {
         int nums[] = {1,2,3,1,2,3,1,2,3,4,5,1,2,1,2,3,12,3};
-        System.out.println(nums.length);
+        majorityElement(nums);
     }
 }
