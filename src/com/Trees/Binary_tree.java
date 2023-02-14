b34160ee65f3c56f45eec1803aff3d864ad445a0
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-14 23:02:25 +0530

    Binary Tree

--- src/com/Trees/Binary_tree.java
+++ src/com/Trees/Binary_tree.java
@@ -5,15 +5,31 @@ public class Binary_tree {
         int data;
         Node left;
         Node right;
-
         Node(int data){
             this.data=data;
             this.left=null;
             this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int index = -1;
 
+        public static Node BuildTree(int[] node){
+            index++;
+            if(node[index]==-1){
+                return null;
+            }
+            Node newNode = new Node(node[index]);
+            newNode.left = BuildTree(node);
+            newNode.right = BuildTree(node);
+
+            return newNode;
         }
     }
     public static void main(String[] args) {
         int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node rootNode = tree.BuildTree(nodes);
+        System.out.println(rootNode.data);
     }
 }
