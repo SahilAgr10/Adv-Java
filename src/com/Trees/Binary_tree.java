6097b470b7a4e7cb2f0acef7d1addf7c207a02c6
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-14 22:34:54 +0530

    Binary Tree

---
+++ src/com/Trees/Binary_tree.java
@@ -0,0 +1,8 @@
+package com.Trees;
+// TODO Pre Order Sequence
+public class Binary_tree {
+
+    public static void main(String[] args) {
+        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    }
+}
