da2b915c4b44220557e3038ed07c333a25d4230d
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-15 22:22:28 +0530

    Sorting

--- src/com/Sorting/CyclicSort/CyclicSort.java
+++ src/com/Sorting/CyclicSort/CyclicSort.java
@@ -1,4 +1,19 @@
 package com.Sorting.CyclicSort;
 
 public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,4,5};
+        // arr = 1,2,3,4,5
+        sort(arr);
+    }
+
+    private static void sort(int[] arr) {
+        for(int i =0;i<arr.length;i++){
+            int correctPosition = i+1;
+            if(arr[i] != correctPosition){
+
+            }
+        }
+    }
+
 }
