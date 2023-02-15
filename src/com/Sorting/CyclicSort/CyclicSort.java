3ef4b5d2872e65451671cf4146a96383be245b7d
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-15 21:56:10 +0530

    Sorting

---
+++ src/com/Sorting/CyclicSort/CyclicSort.java
@@ -0,0 +1,4 @@
+package com.Sorting.CyclicSort;
+
+public class CyclicSort {
+}
--- src/com/Trees/Binary_tree.java
+++ src/com/Trees/Binary_tree.java
@@ -38,7 +38,7 @@ public class Binary_tree {
         if(root == null){
             return;
         }
-        System.out.print(root.data + " ->");
+        System.out.print(root.data + " - > ");
         preOrder(root.left);
         preOrder(root.right);
     }
