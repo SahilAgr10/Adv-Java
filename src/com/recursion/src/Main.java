52332f6d5337dbc34b8fce952d0d18dc41fbd581
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-03-05 23:41:37 +0530

    Sorting

--- src/com/recursion/src/Main.java
+++ src/com/recursion/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {3,6,1,8,5,2};
+        findMax(arr,0,0);
+    }
+    static void findMax(int[] arr,int col,int max){
+        if(col== arr.length){
+            System.out.println(max);
+            return;
+        }
+        if(arr[col] > max){
+            max = arr[col];
+        }
+        findMax(arr, col+1, max);
     }
 }
